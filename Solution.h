2c03f71ecc53d13dedcd470b86e02375508de9bf
VitaliiOnufrienko <bumacka@gmail.com>
2022-05-10 14:31:14 +0300

    add solution

---
+++ Solution.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <vector>
+
+class Solution {
+public:
+    double average(std::vector<int>& salary) {
+        
+        auto index = 0;
+        double result = 0;
+        
+        int min = salary[0];
+        int max = salary[0];
+        
+        
+        while(index < salary.size()){
+            const auto val = salary[index];
+            
+            if (val < min){
+                min = val;
+            }
+            
+            if (val > max){
+                max = val;
+            }
+            
+            result += val;
+            
+            index++;
+        }
+        result = result - min - max;
+        result /= (salary.size() - 2);
+        return result;
+    }
+};
