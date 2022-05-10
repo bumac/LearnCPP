3c9eba7f7e536eab41091e7ba167326eb6b9cb62
VitaliiOnufrienko <bumacka@gmail.com>
2022-05-10 13:14:23 +0300

    solution

--- LearnCPP.cpp
+++ LearnCPP.cpp
@@ -1,6 +1,13 @@
-
-int main(int argc, char* argv[])
-{
-    
-    return 0;
-}
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int count = 0;
+        int currentNumber = low;
+        while (currentNumber <= high){
+            if (currentNumber++ % 2 != 0){
+                ++count;
+            }
+        }
+        return count;
+    }
+};
