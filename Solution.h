c0bd61dc9a612135250b7b69b383d4a5c8dbb2ce
VitaliiOnufrienko <bumacka@gmail.com>
2022-05-10 13:36:18 +0300

    add solution

---
+++ Solution.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        const int delta = high - low + 1;
+        if (delta % 2 == 0){
+            return (delta) / 2;
+        }
+        else{
+            return low % 2 == 0 ? (delta - 1) / 2 : delta / 2 + 1;
+        }
+    }
+};
+
