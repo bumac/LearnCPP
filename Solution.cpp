ebb5d2f61170e40ac986ab1198048bcd770d66c2
VitaliiOnufrienko <bumacka@gmail.com>
2022-05-11 17:19:58 +0300

    resolve conversations

--- Solution.cpp
+++ Solution.cpp
@@ -1,1 +1,22 @@
-﻿#include "Solution.h"
+﻿#include <vector>
+
+class Solution {
+public:
+    double average(const std::vector<int>& salary) {
+        double result = 0;
+        
+        int min = salary[0];
+        int max = salary[0];
+        
+        for (auto val : salary){
+            min = std::min(val, min);
+            max = std::max(val, max);
+            
+            result += val;
+        }
+        
+        result = result - min - max;
+        result /= (salary.size() - 2);
+        return result;
+    }
+};
