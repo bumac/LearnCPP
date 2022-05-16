716e4c87b8b07a5f2c4277fddf2d6895ba193b4d
VitaliiOnufrienko <bumacka@gmail.com>
2022-05-16 19:19:36 +0300

    optimise

--- 1502. Can Make Arithmetic Progression From Sequence.cpp
+++ 1502. Can Make Arithmetic Progression From Sequence.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
-        std::sort(arr.begin(), arr.end());
-        const int delta = arr[1] - arr[0];
-        for(int i = 1; i < arr.size() - 1; ++i){
-            if (arr[i + 1] - arr[i] != delta) return false;
+        
+        std::unordered_set<int> set;
+        set.reserve(arr.size() * 2);
+        auto [min, max] = std::minmax_element(arr.begin(), arr.end());
+        if (*min == *max) return true;
+
+        for(int i = 0; i < arr.size(); i++){
+            if (set.count(arr[i])){
+                return false;
+            }
+            set.insert(arr[i]);
+        }
+        
+        const int delta = (*max - *min) / (arr.size() - 1);
+        int current = *min;
+        for(int i = 0; i < arr.size(); ++i){
+            if (!set.count(current)) {
+                return false;                
+            }
+            current += delta;
         }
         return true;
     }
