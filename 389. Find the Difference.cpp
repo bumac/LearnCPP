63ad5ff364b89783e1528323dee2a50ce0620ebd
VitaliiOnufrienko <bumacka@gmail.com>
2022-05-21 14:58:21 +0300

    update

--- 389. Find the Difference.cpp
+++ 389. Find the Difference.cpp
@@ -3,28 +3,14 @@ public:
     char findTheDifference(string s, string t) {
         if (s.size() == 0) return t[0];
         
-        std::array<int, 26> s_chars;
-        std::array<int, 26> t_chars;
-        
-        for(int i = 0; i < s_chars.size(); ++i){
-            s_chars[i] = 0;
-            t_chars[i] = 0;
-        }
+        char res = 0;
         
         for(int i = 0; i < s.size(); ++i){
-            ++s_chars[s[i] - 'a'];
+            res ^= s[i];
         }
-        
         for(int i = 0; i < t.size(); ++i){
-            ++t_chars[t[i] - 'a'];
+            res ^= t[i];
         }
-        
-        for(int i = 0; i < s_chars.size(); ++i){
-            if (s_chars[i] != t_chars[i]){
-                return 'a' + i;
-            }
-        }
-        
-        return 'a' - 1;
+        return res;
     }
 };
