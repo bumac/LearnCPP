36970cf7093c4327a0b64a8740a9ad2a32e57da2
VitaliiOnufrienko <bumacka@gmail.com>
2022-05-21 14:02:23 +0300

    update

--- 1790. Check if One String Swap Can Make Strings Equal.cpp
+++ 1790. Check if One String Swap Can Make Strings Equal.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        std::vector<int> diffs;
+        std::array<int, 2> arr;
+        int counter = 0;
         for (int i = 0; i < s1.size(); ++i){
-            if (s1[i] != s2[i]) diffs.push_back(i);
+            if (s1[i] != s2[i]) {
+                if (counter == 2) return false;
+                arr[counter++] = i;
+            }
             
         }
         
-        if (diffs.size() == 0) return true;
+        if (counter == 0) return true;
+        if (counter == 1) return false;
         
-        if (diffs.size() == 2){
-            if (s1[diffs[0]] == s2[diffs[1]] && s2[diffs[0]] == s1[diffs[1]])
-                return true;
-        }   
-        
-        return false;
+        return (s1[arr[0]] == s2[arr[1]] && s2[arr[0]] == s1[arr[1]]);
     }
 };
