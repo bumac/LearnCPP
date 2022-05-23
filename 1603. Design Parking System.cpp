f00a1479901acecb74cd1202237d7c3495f19205
VitaliiOnufrienko <bumacka@gmail.com>
2022-05-23 19:35:27 +0300

    1603. Design Parking System

---
+++ 1603. Design Parking System.cpp
@@ -0,0 +1,22 @@
+class ParkingSystem {
+private:
+    std::array<int, 3> parkings;    
+public:
+    ParkingSystem(int big, int medium, int small) : parkings{big, medium, small} {
+        
+    }
+    
+    bool addCar(int carType) {
+        if (parkings[carType - 1] > 0) {
+            --parkings[carType - 1];
+            return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
